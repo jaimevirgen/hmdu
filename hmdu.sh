e5cceaa8cc4eb8bb0d29af97c0304afe4b4196b0
Jaime V <jaimev@mindtouch.com>
2016-05-18 14:45:59 -0700

    first draft of shell script, option definitions and main thread

---
+++ hmdu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Main thread for countdown logic
+main() {
+	add_cal_event
+}
+
+add_cal_event() {
+
+	echo "How many days until…"
+
+}
+
+while getopts "a:d:l" opt;
+do
+  case $opt in
+    a)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      ;;
+    d)
+      echo "-d was triggered, Parameter: $OPTARG" >&2
+      ;;
+    l)
+      echo "-l was triggered, Parameter: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+main
